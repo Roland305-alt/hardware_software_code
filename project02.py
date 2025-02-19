847e18e1ad83a2e00135a597db8291bc21e82f99
Roland305-alt <salcedorolyethan@gmail.com>
2025-02-18 20:51:01 -0500

    H&S classwork 2/18/25

--- project02.py
+++ project02.py
@@ -1,16 +1,30 @@
 def greeting():
-  print("I can only stop if you type exit.")
-  print("This program is designed to have a conversation with the user by asking numerouse of questions")
-  print("I will keep asking questions until you type 'exit'")
-  
-  print("what is your name?")
+    print("Welcome to my program conversation")
+    print("This program is designed to have a conversation with the user by asking numerouse of questions")
+    print("I will keep asking questions until you type 'exit'")
 
-  print("what is your fovorite movie?")
-
-  print("what is your favorite tv show?")
-
-  print("what is your family culture?")
+    print("what is your name?")
+    name = input()
+    if name == "exit":
+      exit()
+    print("what is your favorite movie?")
+    answer = input()
+    if answer == "exit":
+      exit()
+    print("what is your favorite tv show?")
+    answer = input()
+    if answer == "exit":
+      exit()
+    print("what is your family from?")
+    answer = input()
+    if answer == "exit":
+        exit()
+    greeting()
 def main():
   count = 0
-
-  main()
+greeting()
+def exit():
+    print("Thanks for chatting with me Roly Salcedo")
+    quit()
+if __name__== "__main__":
+    main()
